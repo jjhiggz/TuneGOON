a0a1eeb79083667e573665a6c67698c69a9296e7
jjhiggz <jonathan.higger@gmail.com>
2020-10-21 13:19:35 -0600

    first real commit

---
+++ app.js
@@ -0,0 +1,7 @@
+fetch("https://rapidapi.p.rapidapi.com/search?q=eminem", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+		"x-rapidapi-key": "63d0054208msh39803075a61098dp198efbjsn5eb40ada2724"
+	}
+})
